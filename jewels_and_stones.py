9d1b6c3202fd016e5da80b13c4d09bbdd0b41802
RocketBug <my.drive1x@gmail.com>
2020-05-07 13:45:25 +0530

    file updated

---
+++ jewels_and_stones.py
@@ -0,0 +1,12 @@
+#problem url https://leetcode.com/problems/jewels-and-stones/
+from collections import Counter
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        result = 0
+        c = Counter(S)
+        for j in J:
+            if j in c:
+                result += c[j]
+                
+        return result
+                    
