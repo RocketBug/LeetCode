05578d73ed1454c4f2b2d907bb644de36a8afd9d
RocketBug <my.drive1x@gmai.com>
2020-05-12 14:16:37 +0530

    file added

---
+++ single_element_sorted_arr.py
@@ -0,0 +1,34 @@
+# problem url https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3327/
+# similar to single_number.py
+
+# The solution below will work, but it is not O(logN)
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        for j in nums:
+            i = j^i
+    
+        return i
+
+# The solution below is based on mathematics
+# but this solution is also not O(logN)
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        return sum(set(nums))*2-sum(nums)
+
+# The solution below is based on binary search and is O(logN)
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        while True:
+            mid = (low + high) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if mid < high and nums[mid] == nums[mid+1]:
+                low = mid + 2
+            elif mid > low and nums[mid] == nums[mid-1]:
+                high = mid - 2
+            else:
+                return nums[mid]
+
